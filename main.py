fe9c8a24359e023a3395441ffe63e83f12d3e9c8
meowjiao <Meowjiao321@outlook.com>
2023-02-05 15:06:05 +0800

    update

---
+++ main.py
@@ -0,0 +1,37 @@
+def run(code):
+    code_list = code.split("\n")
+    for i in code_list:
+        code_current = ""
+        operation = 0
+        for j in i:
+            if j == " " and operation == 0:
+                if code_current == "print":
+                    operation = 1
+                    code_current = ""
+            elif operation == 1:
+                if j != "\"":
+                    code_current += j
+            else:
+                code_current += j
+        if operation == 1:
+            print(code_current)
+        else:
+            print("error: invalid function")
+
+
+def main():
+    while 1:
+        select = input("run or exit -> ")
+        if select == "run":
+            path = input("File Path -> ")
+            file = open(path, "r")
+            code = file.read()
+            run(code)
+        elif select == "exit":
+            break
+        else:
+            print("error: wrong input")
+
+
+if __name__ == '__main__':
+    main()
