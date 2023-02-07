be04b33cbac3915a5b726af25a77905e65e4b653
meowjiao <Meowjiao321@outlook.com>
2023-02-07 19:36:31 +0800

    update

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 def run(code):
     code_list = code.split("\n")
+    var = {}
     for i in code_list:
         code_current = ""
         operation = 0
+        name = ""
+        print_var = False
+        colon = False
         for j in i:
             if j == " " and operation == 0:
                 if code_current == "print":
                     operation = 1
-                    code_current = ""
+                elif code_current == "var":
+                    operation = 2
+                code_current = ""
             elif operation == 1:
-                if j != "\"":
+                if j == "$" and not colon:
+                    print_var = True
+                elif j == "\"" and colon:
+                    colon = False
+                elif j == "\"":
+                    colon = True
+                else:
                     code_current += j
+            elif operation == 2:
+                if j == "," and not colon:
+                    name = code_current
+                    code_current = ""
+                elif j == "\"" and colon:
+                    colon = False
+                elif j != "\"":
+                    code_current += j
+                    colon = True
             else:
                 code_current += j
         if operation == 1:
-            print(code_current)
+            if print_var:
+                print(var[code_current])
+            else:
+                print(code_current)
+        elif operation == 2:
+            var[name] = code_current
         else:
             print("error: invalid function")
 
